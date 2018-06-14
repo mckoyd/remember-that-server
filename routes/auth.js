096aa6c3957913c7e32084cf64104dfacd1e5b92
mckoyd <dominick.mckoy@gmail.com>
2018-06-13 22:40:07 -0400

    Changed site id

--- routes/auth.js
+++ routes/auth.js
@@ -5,12 +5,13 @@ const { JWT_SECRET, JWT_EXPIRY } = require('../config');
 
 const router = express.Router();
 
-const localAuth = passport.authenticate('local', {session: false, failWithError: true});
-const jwtAuth = passport.authenticate('jwt', { session: false, failWithError: true });
+const localAuth = passport.authenticate('local', {session: false});
+const jwtAuth = passport.authenticate('jwt', { session: false });
 
 const createAuthToken = user => jwt.sign({ user }, JWT_SECRET, {
   subject: user.username,
-  expiresIn: JWT_EXPIRY
+  expiresIn: JWT_EXPIRY,
+  algorithm: 'HS256'
 });
 
 router.post('/login', localAuth, (req, res) => res.json({authToken: createAuthToken(req.user)}));
